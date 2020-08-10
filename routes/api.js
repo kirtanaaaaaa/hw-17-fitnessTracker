55c687178de1eafe4f3b8bb31125bbc449bc94f3
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-08-10 14:32:49 -0400

    deleted Develop folder

---
+++ routes/api.js
@@ -0,0 +1,48 @@
+const db = require("../../models/workout.js");
+const router = require("express").Router();
+
+  router.get("/api/workouts/", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout)
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      })
+    });
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout)
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+    });
+    
+  router.put("/api/workouts/:id", ({ body, params }, res) => {
+      Workout.findByIdAndUpdate(
+        params.id,
+        {$push:{exercises: body}},
+        {new: true}
+      )
+        .then(dbWorkout=> {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        });
+      });
+
+  router.post("/api/workouts/", ({body }, res) => {
+    db.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
